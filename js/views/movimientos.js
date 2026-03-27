e6c898331a63ad6cb9cee07ce73de2c6caa63080
JorgeFresno <jorchdavalos@gmail.com>
2026-03-27 09:39:19 -0600

    Create movimientos.js

---
+++ js/views/movimientos.js
@@ -0,0 +1,15 @@
+export default function () {
+  return `
+    <section class="screen">
+      <div class="screen-header">
+        <h1>Movimientos</h1>
+      </div>
+
+      <div class="screen-body">
+        <div class="card">
+          <p>Pantalla de movimientos lista.</p>
+        </div>
+      </div>
+    </section>
+  `;
+}

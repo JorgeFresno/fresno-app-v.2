6b32c67d95e9e1c6251dd5086286546f1057f272
JorgeFresno <jorchdavalos@gmail.com>
2026-03-27 09:36:02 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+import { renderLayout } from './components/layout.js';
+import { navigate, initRouter } from './router.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app');
+
+  // Cargar estructura base
+  app.innerHTML = renderLayout();
+
+  // Activar router
+  initRouter();
+
+  // Primera vista
+  navigate('movimientos');
+});
